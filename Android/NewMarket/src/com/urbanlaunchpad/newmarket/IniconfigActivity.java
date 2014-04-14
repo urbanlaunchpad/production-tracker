d256003e7f418ec672dfbaa8253e1ca3a36b80c6
buzoherbert <buzoherbert@gmail.com>
2014-04-14 01:22:12 -0700

    Log in screen builds, still needs cleanup.

--- Android/NewMarket/src/com/urbanlaunchpad/newmarket/IniconfigActivity.java
+++ Android/NewMarket/src/com/urbanlaunchpad/newmarket/IniconfigActivity.java
@@ -1,4 +1,4 @@
-package org.urbanlaunchpad.flocktracker;
+package com.urbanlaunchpad.newmarket;
 
 import android.accounts.AccountManager;
 import android.annotation.SuppressLint;
@@ -17,6 +17,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.*;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.RelativeLayout.LayoutParams;
+
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
 import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
 import com.google.api.client.http.HttpTransport;
@@ -27,6 +28,7 @@ import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.fusiontables.Fusiontables;
 import com.google.api.services.fusiontables.Fusiontables.Query.Sql;
 import com.google.api.services.fusiontables.model.Sqlresponse;
+
 import org.apache.http.client.ClientProtocolException;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -53,7 +55,6 @@ public class IniconfigActivity extends Activity implements View.OnClickListener 
     public static GoogleAccountCredential credential;
     public static Fusiontables fusiontables;
     public static SharedPreferences prefs;
-    private static int LOADING_ID = -1;
     TextView usernameField;
     TextView projectNameField;
     ImageView cont;
@@ -103,7 +104,6 @@ public class IniconfigActivity extends Activity implements View.OnClickListener 
         }
     };
     AlertDialog alertDialog;
-    private boolean debison = false; // If true, a test project will be loaded
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -112,7 +112,6 @@ public class IniconfigActivity extends Activity implements View.OnClickListener 
 
         prefs = this.getSharedPreferences("org.urbanlaunchpad.flocktracker",
             Context.MODE_PRIVATE);
-
         // initialize fields
         usernameField = (TextView) findViewById(R.id.usernameText);
         projectNameField = (TextView) findViewById(R.id.projectNameText);
@@ -167,9 +166,6 @@ public class IniconfigActivity extends Activity implements View.OnClickListener 
         projectNameSelectRow.setOnClickListener(this);
         cont.setOnClickListener(this);
 
-        // Debug mode, passes project without internet connection.
-        DebuggingIsOn(debison);
-
         // get credential with scopes
         credential = GoogleAccountCredential.usingOAuth2(this,
             Arrays.asList(FUSION_TABLE_SCOPE, DriveScopes.DRIVE));
@@ -223,10 +219,10 @@ public class IniconfigActivity extends Activity implements View.OnClickListener 
 
 
             // Go to survey
-            Intent i = new Intent(getApplicationContext(), SurveyorActivity.class);
-            i.putExtra("jsonsurvey", jsurv.toString());
-            i.putExtra("username", username);
-            startActivity(i);
+//            Intent i = new Intent(getApplicationContext(), SurveyorActivity.class);
+//            i.putExtra("jsonsurvey", jsurv.toString());
+//            i.putExtra("username", username);
+//            startActivity(i);
         }
     }
 
@@ -346,135 +342,6 @@ public class IniconfigActivity extends Activity implements View.OnClickListener 
     /*
      * Username selection helper functions
      */
-
-    private void DebuggingIsOn(boolean deb) {
-        // Debug mode, passes project without internet connection.
-        // Stuff for debugging without internet connection.
-        if (deb) {
-            projectName = "My fake project is back";
-            username = "fakeuser123@youdontknowwhere.us";
-            try {
-                jsurv = new JSONObject(
-                    "{\"Survey\":{\"Chapters\":[{\"Chapter\":\"SITUACIÓN DEL LOTE Y VIVIENDA\"," +
-                    "\"Questions\":[{\"id\":\"q1\",\"Question\":\"¿Cuál es el uso de su lote?\",\"Kind\":\"OL\"," +
-                    "\"Other\":true,\"Answers\":[{\"Answer\":\"Habitacional\"},{\"Answer\":\"Mixto\"}]}," +
-                    "{\"id\":\"q2\",\"Question\":\"¿Cuál es la superficie del lote?\",\"Kind\":\"LP\"," +
-                    "\"LoopLimit\":\"q6\"},{\"id\":\"q3\",\"Question\":\"¿Cuál es la superficie construida de su " +
-                    "lote?\",\"Kind\":\"ON\"},{\"id\":\"q4\",\"Question\":\"¿Cuántas viviendas est·n construidas en " +
-                    "este predio?\",\"Kind\":\"ON\"},{\"id\":\"q5\",\"Question\":\"¿Cuántas familias comparten esta " +
-                    "vivienda?\",\"Kind\":\"ON\"},{\"id\":\"q6\",\"Question\":\"Este predio es:\",\"Kind\":\"MC\"," +
-                    "\"Other\":true,\"Jump\":\"q6d\",\"Answers\":[{\"Answer\":\"Propio\"},{\"Answer\":\"Rentado 6b\"," +
-                    "\"Jump\":\"q6b\"},{\"Answer\":\"Prestado 6d\"},{\"Answer\":\"Compartido 6d\"}," +
-                    "{\"Answer\":\"Lo cuida 6d\"},{\"Answer\":\"Secesión de derechos 6d\"}," +
-                    "{\"Answer\":\"Otra tenencia 6d\"}]},{\"id\":\"q6a\",\"Question\":\"¿A través de quién " +
-                    "adquirió/rentó/ocupó el lote? 6d\",\"Kind\":\"MC\",\"Other\":true,\"Jump\":\"q6d\"," +
-                    "\"Answers\":[{\"Answer\":\"Fraccionador 6d\"},{\"Answer\":\"Lider 6d\"}," +
-                    "{\"Answer\":\"Comunitario 6d\"},{\"Answer\":\"Ejidatario o comunero 6d\"}," +
-                    "{\"Answer\":\"Funcionario 6d\"}]},{\"id\":\"q6b\",\"Question\":\"Si renta, " +
-                    "¿Cuánto paga mensualmente? 7\",\"Kind\":\"OT\"},{\"id\":\"q6d\"," +
-                    "\"Question\":\"¿Qué documentos de posesión y/o propiedad tiene?\",\"Kind\":\"OT\"}," +
-                    "{\"id\":\"q7\",\"Question\":\"¿Cuánto tiempo lleva viviendo aquí?\",\"Kind\":\"ON\"}," +
-                    "{\"id\":\"q8\",\"Question\":\"¿Está el predio en algún proceso de regularización?\"," +
-                    "\"Kind\":\"MC\",\"Other\":true,\"Answers\":[{\"Answer\":\"Sí\"},{\"Answer\":\"No\"}]}," +
-                    "{\"id\":\"q9\",\"Question\":\"¿Sabe que adquiriÛ un lote en zona no apta para vivienda?\"," +
-                    "\"Kind\":\"MC\",\"Other\":true,\"Answers\":[{\"Answer\":\"SÌ\"},{\"Answer\":\"No\"}]}]}," +
-                    "{\"Chapter\":\"POBREZA DEL TIEMPO LIBRE\",\"Questions\":[{\"id\":\"q29\"," +
-                    "\"Question\":\"¿Cuántos trabajos tiene?\",\"Kind\":\"ON\"},{\"id\":\"q30\"," +
-                    "\"Question\":\"¿Cuántos días a la semana trabaja?\",\"Kind\":\"ON\"},{\"id\":\"q31\"," +
-                    "\"Question\":\"¿Trabaja los fines de semana?\",\"Kind\":\"MC\",\"Other\":false," +
-                    "\"Answers\":[{\"Answer\":\"SÌ\"},{\"Answer\":\"No\"}]},{\"id\":\"q32\"," +
-                    "\"Question\":\"¿A qué hora entra a trabajar?\",\"Kind\":\"ON\"},{\"id\":\"q33\"," +
-                    "\"Question\":\"¿A qué hora sale de trabajar?\",\"Kind\":\"ON\"},{\"id\":\"q34\"," +
-                    "\"Question\":\"¿Cuántas horas al día trabaja?\",\"Kind\":\"ON\"},{\"id\":\"q35\"," +
-                    "\"Question\":\"Cuando tiene tiempo libre, ¿qué hace?\",\"Kind\":\"CB\",\"Other\":false," +
-                    "\"Answers\":[{\"Answer\":\"Convive con la familia\"},{\"Answer\":\"Trata de descansar o " +
-                    "dormir\"},{\"Answer\":\"Hace cualquier cosa dentro de su casa\"}," +
-                    "{\"Answer\":\"Hace alguna actividad recreativa\"},{\"Answer\":\"Ir al cine\"}," +
-                    "{\"Answer\":\"Ir al teatro\"},{\"Answer\":\"Ir al museo\"},{\"Answer\":\"Ir al parque\"}," +
-                    "{\"Answer\":\"Ir a convivir con familiares cercanos\"},{\"Answer\":\"Ir a convivir con " +
-                    "amigos\"},{\"Answer\":\"Leer un libro\"},{\"Answer\":\"Navegar en internet\"}," +
-                    "{\"Answer\":\"Ver televisión\"}]}]},{\"Chapter\":\"ORGANIZACIÓN VECINAL\"," +
-                    "\"Questions\":[{\"id\":\"q36\",\"Question\":\"Como comunidad, " +
-                    "¿Cómo es que resuelven sus carencias, como?\",\"Kind\":\"MC\",\"Other\":true,\"Jump\":\"q36a\"," +
-                    "\"Answers\":[{\"Answer\":\"Agua 36a\"},{\"Answer\":\"Pavimentación 36a\"}," +
-                    "{\"Answer\":\"Electricidad 36a\"},{\"Answer\":\"Regularización de la tenencia de la tierra " +
-                    "36a\"}]},{\"id\":\"q36a\",\"Question\":\"¿Quién se encarga?\",\"Kind\":\"OT\"},{\"id\":\"q37\"," +
-                    "\"Question\":\"¿Dejan que se incorporen al asentamiento nuevos vecinos?\",\"Kind\":\"MC\"," +
-                    "\"Other\":false,\"Jump\":\"q37a\",\"Answers\":[{\"Answer\":\"Si 37a\"}," +
-                    "{\"Answer\":\"No 37a\"}]},{\"id\":\"q37a\",\"Question\":\"¿Por qué?\",\"Kind\":\"OT\"}," +
-                    "{\"id\":\"q38\",\"Question\":\"¿En este asentamiento tiene algún familiar?\",\"Kind\":\"MC\"," +
-                    "\"Other\":false,\"Answers\":[{\"Answer\":\"Si\"},{\"Answer\":\"No\"}]},{\"id\":\"q39\"," +
-                    "\"Question\":\"¿Alguien se encarga de organizarlos?\",\"Kind\":\"MC\",\"Other\":false," +
-                    "\"Jump\":\"q39a\",\"Answers\":[{\"Answer\":\"Si 39a\"},{\"Answer\":\"No\"}]},{\"id\":\"q39a\"," +
-                    "\"Question\":\"¿Quién?\",\"Kind\":\"OT\"},{\"id\":\"q40\"," +
-                    "\"Question\":\"Para lograr sus demandas de servicios como agua, luz, pavimentación, " +
-                    "vivienda. ¿A dónde acuden?\",\"Kind\":\"MC\",\"Other\":false," +
-                    "\"Answers\":[{\"Answer\":\"Coordinación Territorial\"},{\"Answer\":\"La Delegación\"}," +
-                    "{\"Answer\":\"GDF\"},{\"Answer\":\"Partido Político\"}]},{\"id\":\"q41\"," +
-                    "\"Question\":\"¿Les han resuelto sus demandas?\",\"Kind\":\"MC\",\"Other\":false," +
-                    "\"Jump\":\"q41a\",\"Answers\":[{\"Answer\":\"Si 41a\"},{\"Answer\":\"No 41b\"}]}," +
-                    "{\"id\":\"q41a\",\"Question\":\"¿Cuáles?\",\"Kind\":\"MC\",\"Other\":false," +
-                    "\"Answers\":[{\"Answer\":\"Agua\"},{\"Answer\":\"Pavimentación\"},{\"Answer\":\"Electricidad\"}," +
-                    "{\"Answer\":\"Regularización y tenencia de la tierra\"}]},{\"id\":\"q41b\"," +
-                    "\"Question\":\"¿Por qué?\",\"Kind\":\"OT\"},{\"id\":\"q42\",\"Question\":\"¿Se han visto en la " +
-                    "necesidad de que ustedes mismos las resuelvan?\",\"Kind\":\"MC\"," +
-                    "\"Answers\":[{\"Answer\":\"Sí\"},{\"Answer\":\"No\"}]},{\"id\":\"q43\"," +
-                    "\"Question\":\"¿Cuentan con una organización en la colonia?\",\"Kind\":\"MC\",\"Other\":false," +
-                    "\"Jump\":\"q44\",\"Answers\":[{\"Answer\":\"Sí\"},{\"Answer\":\"No 44\"}]},{\"id\":\"q43a\"," +
-                    "\"Question\":\"¿Cuales son las funciones del líder vecinal respecto a la colonia?\"," +
-                    "\"Kind\":\"OT\"},{\"id\":\"q43b\",\"Question\":\"¿Que logros han conseguido a través de la " +
-                    "organización?\",\"Kind\":\"MC\",\"Other\":false,\"Answers\":[{\"Answer\":\"Servicios Urbanos " +
-                    "(pavimentación, agua, luz, etc.)\"},{\"Answer\":\"Regularización de la tenencia de la tierra\"}," +
-                    "{\"Answer\":\"Servicios sociales (educación-salud)\"}]},{\"id\":\"q43c\"," +
-                    "\"Question\":\"¿Con que frecuencia tienen reuniones?\",\"Kind\":\"MC\",\"Other\":false," +
-                    "\"Answers\":[{\"Answer\":\"Semanal\"},{\"Answer\":\"Mensual\"}]},{\"id\":\"q44\"," +
-                    "\"Question\":\"Usted, ¿Qué tan seguro se siente en este lugar?\",\"Kind\":\"MC\"," +
-                    "\"Other\":false,\"Jump\":\"q45\",\"Answers\":[{\"Answer\":\"Seguro 45\"}," +
-                    "{\"Answer\":\"Medio seguro\"},{\"Answer\":\"Inseguro\"}]},{\"id\":\"q45\"," +
-                    "\"Question\":\"¿Cómo percibes la seguridad en este lugar?\",\"Kind\":\"MC\",\"Other\":false," +
-                    "\"Jump\":\"q46a\",\"Answers\":[{\"Answer\":\"Buena 46a\"},{\"Answer\":\"Regular\"}," +
-                    "{\"Answer\":\"Pésima\"}]},{\"id\":\"q46\",\"Question\":\"¿Por qué regular o pésima?\"," +
-                    "\"Kind\":\"MC\",\"Other\":false,\"Jump\":\"q47\",\"Answers\":[{\"Answer\":\"Por ausencia de " +
-                    "pratullas 47\"},{\"Answer\":\"Por ausencia de policías o pie o en bicicleta 47\"}," +
-                    "{\"Answer\":\"Ausencia de Policias en motocicleta 47\"}]},{\"id\":\"q46a\"," +
-                    "\"Question\":\"¿Por qué buena?\",\"Kind\":\"MC\",\"Other\":false," +
-                    "\"Answers\":[{\"Answer\":\"Por que hay patrullas\"},{\"Answer\":\"Por la presencia de policias a" +
-                    " pie o en bicicleta\"},{\"Answer\":\"Presencia de policia en bicicleta\"}]},{\"id\":\"q47\"," +
-                    "\"Question\":\"¿Se organizan para que ustedes mismos vigilen su calle?\",\"Kind\":\"MC\"," +
-                    "\"Other\":false,\"Jump\":\"q48\",\"Answers\":[{\"Answer\":\"Sí\"},{\"Answer\":\"No 48\"}]}," +
-                    "{\"id\":\"q47a\",\"Question\":\"¿Cómo se organizan?\",\"Kind\":\"MC\",\"Other\":true," +
-                    "\"Answers\":[{\"Answer\":\"Hacen guardias\"},{\"Answer\":\"Hacen rondines\"}," +
-                    "{\"Answer\":\"Ponen alarmas\"}]}]},{\"Chapter\":\"RIESGOS Y AMENAZAS\"," +
-                    "\"Questions\":[{\"id\":\"q48\",\"Question\":\"¿Que tipo de riesgos tienen en esta zona?\"," +
-                    "\"Kind\":\"MC\",\"Other\":true,\"Answers\":[{\"Answer\":\"Inundaciones\"}," +
-                    "{\"Answer\":\"Deslizamiento de tierra\"},{\"Answer\":\"Incendios forestales\"}," +
-                    "{\"Answer\":\"Desalojos\"},{\"Answer\":\"Otros\"},{\"Answer\":\"Ninguno\"}]},{\"id\":\"q49\"," +
-                    "\"Question\":\"¿A que riesgo se siente más vulnerable?\",\"Kind\":\"MC\",\"Other\":true," +
-                    "\"Answers\":[{\"Answer\":\"Inundaciones\"},{\"Answer\":\"Deslizamiento de tierra\"}," +
-                    "{\"Answer\":\"Incendios forestales\"},{\"Answer\":\"Desalojos\"}," +
-                    "{\"Answer\":\"Infraestructura física\"},{\"Answer\":\"Otros\"},{\"Answer\":\"Ninguno\"}]}," +
-                    "{\"id\":\"q50\",\"Question\":\"¿Que tipo de obras se han llevado a cabo para mejorar este lugar " +
-                    "contra los riesgos?\",\"Kind\":\"MC\",\"Other\":true,\"Answers\":[{\"Answer\":\"Poner muros de " +
-                    "contención\"},{\"Answer\":\"Costaleras\"},{\"Answer\":\"Lonas antiderrumbes\"}]}," +
-                    "{\"id\":\"q51\",\"Question\":\"¿Como las han logrado?\",\"Kind\":\"CB\",\"Other\":false," +
-                    "\"Answers\":[{\"Answer\":\"Acciones individuales y familiares\"}," +
-                    "{\"Answer\":\"Obras vecinales/comunitarias\"},{\"Answer\":\"Obras del Gobierno " +
-                    "delegacional\"}]}]}],\"TableID\":\"11lGsm8B2SNNGmEsTmuGVrAy1gcJF9TQBo3G1Vw0\"}," +
-                    "\"TrackerAlarm\":{\"Questions\":[{\"id\":\"q1\",\"Question\":\"Camión\",\"Kind\":\"MC\"," +
-                    "\"Other\":true,\"Answers\":[{\"Answer\":\"Ruta 56\"},{\"Answer\":\"Ruta 15\"}]},{\"id\":\"q2\"," +
-                    "\"Question\":\"Número de pasajeros.\",\"Kind\":\"ON\",\"Other\":true}]," +
-                    "\"TableID\":\"1Q2mr8ni5LTxtZRRi3PNSYxAYS8HWikWqlfoIUK4\"}}"
-                );
-            } catch (JSONException e2) {
-                e2.printStackTrace();
-            }
-            messageHandler.sendEmptyMessage(EVENT_TYPE.PARSED_CORRECTLY
-                .ordinal());
-            messageHandler.sendEmptyMessage(EVENT_TYPE.GOT_USERNAME.ordinal());
-            messageHandler.sendEmptyMessage(EVENT_TYPE.GOT_PROJECT_NAME
-                .ordinal());
-        }
-    }
 
     private enum EVENT_TYPE {
         GOT_USERNAME, GOT_PROJECT_NAME, PARSED_CORRECTLY, PARSED_INCORRECTLY, INPUT_NAME

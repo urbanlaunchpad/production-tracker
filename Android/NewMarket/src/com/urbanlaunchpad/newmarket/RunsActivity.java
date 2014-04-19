76341774f4c8955dbe993d7eeb5b85517d96cdc4
buzoherbert <buzoherbert@gmail.com>
2014-04-19 16:25:16 -0700

    merge

--- Android/NewMarket/src/com/urbanlaunchpad/newmarket/RunsActivity.java
+++ Android/NewMarket/src/com/urbanlaunchpad/newmarket/RunsActivity.java
@@ -13,9 +13,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
 import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
